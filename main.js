3320c407eb223942937bfc63564790fa1e1ae7a6
Jeffrey Heller <heller.jeffrey@gmail.com>
2018-02-09 14:32:20 -0500

    added createGame function/more pseudocode

--- main.js
+++ main.js
@@ -1,1 +1,36 @@
+//js test
 console.log('js working')
+console.log(document.querySelector('#columnone'))
+//createGame function - called on page load
+
+//class Block to create 5 levels of pyramid
+class Block {
+    constructor(size, position) {
+        this.size = size,
+        position = 1
+    }
+
+    //add moveBlock function as method for new objects
+}
+
+// checked - successfully creates 5 new divs
+function createGame () {
+    let blockArray = ["top", "second", "third", "fourth", "bottom"]
+    for (i = 0; i < blockArray.length; i++ ) {
+    var newBlock = document.createElement('div')
+    const block1 = new Block(size = i, position = 1)
+    let startColumn = document.querySelector('#columnone')
+    startColumn.appendChild(newBlock)
+    newBlock.classList = ("block " + blockArray[i])
+    }
+}
+createGame()
+
+//move block function - moving blocks between columns
+function moveBlock () {
+    console.log('moveBlock function working')
+    //add check for win condition
+}
+moveBlock()
+
+//win condition function to be called by moveBlock
