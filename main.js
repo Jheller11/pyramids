6859586963b7f7991c6ee783973acd76579342f4
Jeffrey Heller <heller.jeffrey@gmail.com>
2018-02-10 21:59:28 -0500

    added class column

--- main.js
+++ main.js
@@ -1,34 +1,61 @@
 // js test
 console.log('js working')
+
+const column1div = document.querySelector('#columnone')
+const column2div = document.querySelector('#columntwo')
+const column3div = document.querySelector('#columnthree')
 //
-const column1 = document.querySelector('#columnone')
-const column2 = document.querySelector('#columntwo')
-const column3 = document.querySelector('#columnthree')
-const column1Array = []
-const column2Array = []
-const column3Array = []
+class Column {
+  constructor (name) {
+    this.name = name
+  }
+
+  addBlock () {
+    let currentColumn = (this.name + 'div')
+    console.log(currentColumn)
+    currentColumn.appendChild(block1div)
+    console.log('addBlock method working')
+  }
+
+    // move block function - moving blocks between columns
+  moveBlock () {
+    let blockname = block1div
+    let target = parseInt(prompt('What column would you like to move to block to?'))
+    if (target === 1) {
+      column1div.appendChild(blockname)
+    } else if (target === 2) {
+      column2div.appendChild(blockname)
+    } else if (target === 3) {
+      column3div.appendChild(blockname)
+    }
+    console.log('move function working')
+  }
+}
+const column1 = new Column('column1')
+const column2 = new Column('column2')
+const column3 = new Column('column3')
+
 
 class Block {
-    constructor(name, position, size) {
-        this.name = name
-        this.position = position
-        this.size = size
-    }
+  constructor (name, position, size) {
+    this.name = name
+    this.position = position
+    this.size = size
+  }
 }
 const block1 = new Block('block1', 1, 1)
 const block2 = new Block('block2', 1, 2)
 const block3 = new Block('block3', 1, 3)
 const block4 = new Block('block4', 1, 4)
 const block5 = new Block('block5', 1, 5)
-   
+
 const blockArray = [block1, block2, block3, block4, block5]
 
-//checked - successfully creates 5 new divs
+// checked - successfully creates 5 new divs
 function createGame () {
   for (i = 0; i < blockArray.length; i++) {
     var newBlock = document.createElement('div')
-    column1.appendChild(newBlock)
-    column1Array.push(newBlock)
+    column1div.appendChild(newBlock)
     newBlock.classList = ('block ' + blockArray[i].name)
   }
   console.log('createGame working')
@@ -44,25 +71,10 @@ const block5div = document.querySelector('.block5')
 addListeners()
 
 function addListeners () {
-  column1.addEventListener('click', moveBlock)
-  column2.addEventListener('click', moveBlock)
-  column3.addEventListener('click', moveBlock)
+  column1div.addEventListener('click', column1.moveBlock)
+  column2div.addEventListener('click', column2.moveBlock)
+  column3div.addEventListener('click', column3.moveBlock)
   console.log('listeners added')
-}
-
-// move block function - moving blocks between columns
-function moveBlock (blockname, column) {
-  if (column === 1) {
-      column1.appendChild(blockname)
-      column1Array.push(blockname)
-    } else if (column === 2) {
-      column2.appendChild(blockname)
-      column2Array.push(blockname)
-    } else if (column === 3) {
-      column3.appendChild(blockname)
-      column3Array.push(blockname)
-    }
-  console.log('move function working - this is where checkWin will go')
 }
 
 // functions for sorting blocks after each move
@@ -70,6 +82,6 @@ function moveBlock (blockname, column) {
 // win condition function to be called at end of  moveBlock
 function checkWin () {
   if (column3.childNodes.length === 5) {
-      alert('you won')
-    }
+    alert('you won')
+  }
 }
