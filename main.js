7b95aee4414a184daf808fea88f0088f65cbe162
Jeffrey Heller <heller.jeffrey@gmail.com>
2018-02-12 11:29:20 -0500

    game logic working with check for valid move and check for win condition

--- main.js
+++ main.js
@@ -1,41 +1,3 @@
-// js test
-console.log('js working')
-
-const column1div = document.querySelector('#columnone')
-const column2div = document.querySelector('#columntwo')
-const column3div = document.querySelector('#columnthree')
-//
-class Column {
-  constructor (name) {
-    this.name = name
-  }
-
-  addBlock () {
-    let currentColumn = (this.name + 'div')
-    console.log(currentColumn)
-    currentColumn.appendChild(block1div)
-    console.log('addBlock method working')
-  }
-
-    // move block function - moving blocks between columns
-  moveBlock () {
-    let blockname = block1div
-    let target = parseInt(prompt('What column would you like to move to block to?'))
-    if (target === 1) {
-      column1div.appendChild(blockname)
-    } else if (target === 2) {
-      column2div.appendChild(blockname)
-    } else if (target === 3) {
-      column3div.appendChild(blockname)
-    }
-    console.log('move function working')
-  }
-}
-const column1 = new Column('column1')
-const column2 = new Column('column2')
-const column3 = new Column('column3')
-
-
 class Block {
   constructor (name, position, size) {
     this.name = name
@@ -49,18 +11,30 @@ const block3 = new Block('block3', 1, 3)
 const block4 = new Block('block4', 1, 4)
 const block5 = new Block('block5', 1, 5)
 
-const blockArray = [block1, block2, block3, block4, block5]
+const game = {
+  column1Array: [block1, block2, block3, block4, block5],
+  column2Array: [],
+  column3Array: []
+}
+var activeBlock = 'test'
+var toColumn = 'test'
+var fromColumn = 'test'
+
+const column1div = document.querySelector('#columnone')
+const column2div = document.querySelector('#columntwo')
+const column3div = document.querySelector('#columnthree')
 
 // checked - successfully creates 5 new divs
 function createGame () {
-  for (i = 0; i < blockArray.length; i++) {
+  for (i = 0; i < game.column1Array.length; i++) {
     var newBlock = document.createElement('div')
     column1div.appendChild(newBlock)
-    newBlock.classList = ('block ' + blockArray[i].name)
+    newBlock.classList = ('block ' + game.column1Array[i].name)
   }
   console.log('createGame working')
 }
 createGame()
+move()
 
 const block1div = document.querySelector('.block1')
 const block2div = document.querySelector('.block2')
@@ -68,20 +42,140 @@ const block3div = document.querySelector('.block3')
 const block4div = document.querySelector('.block4')
 const block5div = document.querySelector('.block5')
 
-addListeners()
-
-function addListeners () {
-  column1div.addEventListener('click', column1.moveBlock)
-  column2div.addEventListener('click', column2.moveBlock)
-  column3div.addEventListener('click', column3.moveBlock)
-  console.log('listeners added')
+function move () {
+  fromListeners()
 }
 
-// functions for sorting blocks after each move
+function fromListeners () {
+  column1div.addEventListener('click', column1)
+  column2div.addEventListener('click', column2)
+  column3div.addEventListener('click', column3)
+  console.log('from listeners added')
+}
+
+function toListeners () {
+  column1div.addEventListener('click', tocolumn1)
+  column2div.addEventListener('click', tocolumn2)
+  column3div.addEventListener('click', tocolumn3)
+  console.log('to listeners added')
+}
 
 // win condition function to be called at end of  moveBlock
 function checkWin () {
-  if (column3.childNodes.length === 5) {
-    alert('you won')
+  if (column3div.childNodes.length === 5) {
+    return true
   }
+}
+
+function updateColumns () {
+  deletedivs()
+  updateColumn1()
+  updateColumn2()
+  updateColumn3()
+  console.log('columns update')
+}
+function updateColumn1 () {
+  for (i = 0; i < game.column1Array.length; i++) {
+    var newBlock = document.createElement('div')
+    column1div.appendChild(newBlock)
+    newBlock.classList = ('block ' + game.column1Array[i].name)
+  }
+}
+function updateColumn2 () {
+  for (i = 0; i < game.column2Array.length; i++) {
+    var newBlock = document.createElement('div')
+    column2div.appendChild(newBlock)
+    newBlock.classList = ('block ' + game.column2Array[i].name)
+  }
+}
+function updateColumn3 () {
+  for (i = 0; i < game.column3Array.length; i++) {
+    var newBlock = document.createElement('div')
+    column3div.appendChild(newBlock)
+    newBlock.classList = ('block ' + game.column3Array[i].name)
+  }
+}
+function deletedivs () {
+  while (column1div.firstChild) {
+    column1div.removeChild(column1div.firstChild)
+  }
+  while (column2div.firstChild) {
+    column2div.removeChild(column2div.firstChild)
+  }
+  while (column3div.firstChild) {
+    column3div.removeChild(column3div.firstChild)
+  }
+}
+
+function removeListeners () {
+  column1div.removeEventListener('click', column1)
+  column2div.removeEventListener('click', column2)
+  column3div.removeEventListener('click', column3)
+  column1div.removeEventListener('click', tocolumn1)
+  column2div.removeEventListener('click', tocolumn2)
+  column3div.removeEventListener('click', tocolumn3)
+}
+
+function column1 () {
+  console.log('column1')
+  removeListeners()
+  activeBlock = game.column1Array[0]
+  fromColumn = game.column1Array
+  toListeners()
+}
+function column2 () {
+  console.log('column2')
+  removeListeners()
+  activeBlock = game.column2Array[0]
+  fromColumn = game.column2Array
+  toListeners()
+}
+function column3 () {
+  console.log('column3')
+  removeListeners()
+  activeBlock = game.column3Array[0]
+  fromColumn = game.column3Array
+  toListeners()
+}
+function tocolumn1 () {
+  console.log('column1')
+  removeListeners()
+  toColumn = game.column1Array
+  pushToColumn()
+}
+function tocolumn2 () {
+  console.log('column2')
+  removeListeners()
+  toColumn = game.column2Array
+  pushToColumn()
+}
+function tocolumn3 () {
+  console.log('column2')
+  removeListeners()
+  toColumn = game.column3Array
+  pushToColumn()
+}
+function pushToColumn () {
+  if (toColumn[0]) {
+    if (activeBlock.size < toColumn[0].size) {
+  toColumn.unshift(activeBlock)
+  fromColumn.splice(0, 1)
+  updateColumns()
+  if (checkWin() === true) {
+    alert('You won')
+  }
+  move()
+} else {
+  alert('invalid move')
+  move()
+}
+} else {
+  toColumn.unshift(activeBlock)
+  fromColumn.splice(0, 1)
+  updateColumns()
+  if (checkWin() === true) {
+    alert('You won')
+  }
+  move()
+}
 }
